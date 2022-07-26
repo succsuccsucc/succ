8c9ac39e54aeec60f1d50c58bf09aa925745a621
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2022-07-26 14:35:30 +0800

    ?mtrfare: added warning for same station trips

--- bot.py
+++ bot.py
@@ -264,7 +264,25 @@ async def mtrfare(ctx, start, end):
             embed_mtrfare.add_field(name='Elderly (Ticket)', value=single_con_elderly_fare, inline=True)
             embed_mtrfare.add_field(name='Disability (Octopus)', value=oct_con_pwd_fare, inline=False)
 
-            mtrfare_footer = 'Students aged 12 and above travelling with Single Journey Tickets are required to pay full adult fare.\nEligible persons with disabilities aged 12 to 64 have to pay adult fares.'
+            mtrfare_footer = 'Students aged 12 and above and eligible persons with disabilities aged 12 to 64 travelling with Single Journey Tickets are required to pay full adult fare.'
+            
+            if fare_list[i][0].upper() == fare_list[i][2].upper():
+                embed_mtrfare.clear_fields()
+                same_station = 'Entrance within 20 minute: Paying minimum fare of single ride ticket\n\
+                                Within 150 minute: $10 for adult, $5 for Child, Minimum fare of single ride ticket for elderly and PwD\n\
+                                After 150 minutes: Paying maximum fare of single ride ticket\n\n\
+                                https://cdn.discordapp.com/attachments/805744932975280158/1001367948604223508/unknown.png'               
+                
+                if (fare_list[i][0] == 'Lo Wu') or (fare_list[i][0] == 'Lok Ma Chau'):
+                    same_station = 'Lo Wu/Lok Ma Chau Regulation\n\
+                                    Within 20 minutes: Minimum single ride ticket fare of that station\n\
+                                    Within 150 minutes: Except elderlies, PwD and Concessionary Travel Scheme user,\n\
+                                    - First class user (Using first class single ride ticket/Tapped the first class processor/Using QR code, selected first class): Minimum fee of the first class, i.e. Minimum of adult/concessionary fare and first class premium of that station.\n\
+                                    - Others: Current minimum fare for a single journey of that station. However, concessionary Travel Scheme user not applicable.\n\n\
+                                    https://cdn.discordapp.com/attachments/805744932975280158/1001367948604223508/unknown.png'
+                
+                embed_mtrfare.add_field(name='Same station entry and exit', value=same_station, inline=False)
+
             embed_mtrfare.set_footer(text=mtrfare_footer)
 
             await ctx.send(embed=embed_mtrfare)
@@ -282,6 +300,7 @@ async def help(ctx):
     embed_help.add_field(name='?kmbtest <stop_name>', value='Tests if a bus stop with the given name exists.', inline=False)
     embed_help.add_field(name='?kmbeta <stop_name>', value='Gets ETA of all KMB routes at a bus stop.', inline=False)
     embed_help.add_field(name='?light <stop_name>', value='Gets train arrival times at a Light Rail stop.', inline=False)
+    embed_help.add_field(name='?mtrfare <start> <end>', value='Gets the MTR fare between two stations.', inline=False)
 
     command_count = str(len(embed_help.fields))
     footer_string = f'Total {command_count} commands.'
