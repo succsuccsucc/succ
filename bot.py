c003bd37442d927b10bf1213b25b94e3bce1ddfa
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2022-07-28 22:00:32 +0800

    added ?mtrbus added ?mtrbus to ?help message

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # bot.py
+from http.client import HTTPException
 import os
 import time
 import requests
@@ -30,7 +31,7 @@ kmb_stops = requests.request("GET", "https://data.etabus.gov.hk/v1/transport/kmb
 kmb_stops = kmb_stops.json()
 
 # Open list of Light Rail stops
-lrt = open('data/light_rail_stops.json', encoding="utf-8",)
+lrt = open('data/light_rail_stops.json', encoding='utf-8')
 light_rail_stops = json.load(lrt)
 
 # Open MTR fares CSV
@@ -39,6 +40,19 @@ datareader = csv.reader(mtr_fares, delimiter=',')
 fare_list = []
 for row in datareader:
     fare_list.append(row)
+
+# Open lists of MTR bus routes and stops
+mtr_bus_stops = open('data/mtr_bus_stops.csv', 'r', encoding='utf-8')
+datareader_mtr_bus = csv.reader(mtr_bus_stops, delimiter=',')
+mtr_bus_stops_list = []
+for row in datareader_mtr_bus:
+    mtr_bus_stops_list.append(row)
+
+mtr_bus_routes = open('data/mtr_bus_routes.csv', 'r', encoding='utf-8')
+datareader_mb_routes = csv.reader(mtr_bus_routes, delimiter=',')
+mb_routes = []
+for row in datareader_mb_routes:
+    mb_routes.append(row)
 
 @client.event
 async def on_ready():
@@ -286,8 +300,90 @@ async def mtrfare(ctx, start, end):
             await ctx.send(embed=embed_mtrfare)
             return
 
-    await ctx.send('Incorrect start or destination!')
+    await ctx.send('Start or destination does not exist!')
 
+@commands.cooldown(1, 5, commands.BucketType.guild)
+@client.command()
+async def mtrbus(ctx, route):
+    route = route.upper()
+    
+    for i in range(len(mb_routes)):
+        if route == mb_routes[i][0] or route == '506':
+            url = 'https://rt.data.gov.hk/v1/transport/mtr/bus/getSchedule'
+            mb_response = requests.post(url, json={"language": "en", "routeName": route})
+            mb_response = mb_response.json()
+
+            if len(mb_response['busStop']) == 0:
+                await ctx.send('Route is not in service!')
+                return
+
+            mb_title = mb_response['routeName']
+            mb_description = mb_routes[i][1]
+            embed_mtrbus = discord.Embed(title=mb_title, description=mb_description, color=0xffffff)
+            
+            stop_field = ''
+            time_field = ''
+            for a in range(len(mb_response['busStop'])):
+                bus_stop_id = mb_response['busStop'][a]['busStopId']
+                
+                time_field += mb_response['busStop'][a]['bus'][0]['departureTimeText']
+                time_field += '\n'
+
+                for b in range(len(mtr_bus_stops_list)):
+                    if bus_stop_id == mtr_bus_stops_list[b][5]: # Stop ID on column A are untrimmed
+                        if mtr_bus_stops_list[b + 1][5] == '':  # Make last stops bold
+                            stop_field = stop_field + '**_' + mtr_bus_stops_list[b][4] + '_**'
+                        else:
+                            stop_field += mtr_bus_stops_list[b][4]
+                        break
+                    
+                stop_field += '\n' 
+            
+            stop_count = stop_field.count('\n')
+
+            stop_field_list = stop_field.split('\n')
+            time_field_list = time_field.split('\n')
+
+            stop_field_slice = []
+            time_field_slice = []
+
+            c = 0
+ 
+            while c < len(stop_field_list) - 1:
+                stop_field_send = ''
+                time_field_send = ''
+
+                stop_field_slice = stop_field_list[c : c + 11] 
+                time_field_slice = time_field_list[c : c + 11]
+
+                for d in range(len(stop_field_slice)):
+                    stop_field_send += stop_field_slice[d]
+                    stop_field_send += '\n'
+                for e in range(len(time_field_slice)):
+                    time_field_send += time_field_slice[e]
+                    time_field_send += '\n'
+
+                embed_mtrbus.add_field(name='Stop', value=stop_field_send, inline=True)
+                embed_mtrbus.add_field(name='Time', value=time_field_send, inline=True)
+
+                if stop_count < (c + 11):
+                    split_footer = f'Stops {c + 1} to {stop_count}. Total {stop_count} stops.'
+                else:
+                    split_footer = f'Stops {c + 1} to {c + 11}. Total {stop_count} stops.'
+
+                split_footer += '\nLast stops on the route are bolded and italicized.'
+
+                embed_mtrbus.set_footer(text=split_footer)
+
+                await ctx.send(embed=embed_mtrbus)
+                embed_mtrbus.clear_fields()
+
+                c += 11
+
+            return
+
+    await ctx.send('Route does not exist!')                
+                
 @client.command()
 async def help(ctx):
     embed_help = discord.Embed(title='Help!', description='', color=0xcca6fd)
@@ -299,6 +395,7 @@ async def help(ctx):
     embed_help.add_field(name='?kmbeta <stop_name>', value='Gets ETA of all KMB routes at a bus stop.', inline=False)
     embed_help.add_field(name='?light <stop_name>', value='Gets train arrival times at a Light Rail stop.', inline=False)
     embed_help.add_field(name='?mtrfare <start> <end>', value='Gets the MTR fare between two stations.', inline=False)
+    embed_help.add_field(name='?mtrbus <route>', value='Gets ETA at all bus stops of an MTR Bus route.', inline=False)
 
     command_count = str(len(embed_help.fields))
     footer_string = f'Total {command_count} commands.'
